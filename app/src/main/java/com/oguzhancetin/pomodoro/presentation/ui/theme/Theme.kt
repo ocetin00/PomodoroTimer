21e1a694593be83386807d684a3c13911c67fe43
ocetin14 <ocetin169@gmail.com>
2023-06-13 18:39:29 +0300

    fix status bar color

--- app/src/main/java/com/oguzhancetin/pomodoro/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/oguzhancetin/pomodoro/presentation/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.oguzhancetin.pomodoro.presentation.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,10 +11,13 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
@@ -86,19 +90,44 @@ fun PomodoroTheme(
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) {
+                dynamicDarkColorScheme(context)
+
+            } else {
+                dynamicLightColorScheme(context)
+            }
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
+
+    val backgroundColor = colorScheme.primaryContainer
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val window = (view.context as Activity).window
+            window.statusBarColor = backgroundColor.toArgb()
+
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+   SideEffect {
+        if(darkTheme){
+            systemUiController.setSystemBarsColor(
+                color = backgroundColor
+            )
+        }else{
+            systemUiController.setSystemBarsColor(
+                color = backgroundColor
+            )
         }
     }
 
